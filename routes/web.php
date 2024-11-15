43823497fd95fae7a455df1834e573ed9389f5ca
kent <melquiadeskentcarlo@gmail.com>
2024-11-15 16:58:34 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return 'cool';
 });
 
 Route::get('/clear-cache', function () {
-    Artisan::call('vendor:publish --force --tag=livewire:assets');
+    // Clear route cache
+    Artisan::call('route:clear');
+    $routeOutput = Artisan::output();
+
+    // Clear view cache
+    Artisan::call('view:clear');
+    $viewOutput = Artisan::output();
+
+    // Clear config cache
+    Artisan::call('config:clear');
+    $configOutput = Artisan::output();
+
+    return response()->json([
+        'message' => 'Caches cleared successfully',
+        'outputs' => [
+            'route' => $routeOutput,
+            'view' => $viewOutput,
+            'config' => $configOutput,
+        ]
+    ]);
 });
