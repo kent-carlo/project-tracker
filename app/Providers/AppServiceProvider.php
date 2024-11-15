2b6e66b0f36a7622b8567469a49a260ea5e31665
kent <melquiadeskentcarlo@gmail.com>
2024-11-15 17:41:52 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\URL;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -34,5 +36,8 @@ class AppServiceProvider extends ServiceProvider
                 }
             }         
         }
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        };
     }
 }
