52c619365913c39c019eba950d2301f33921065d
kent <melquiadeskentcarlo@gmail.com>
2024-11-15 17:33:06 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (config('app.vercel.enabled')) {
+            $paths = [
+                '/tmp/framework/sessions',
+                '/tmp/framework/cache',
+                '/tmp/storage/bootstrap/cache',
+                '/tmp/storage/framework/cache',
+                config('view.compiled'),
+            ];
+
+            foreach ($paths as $path) {
+                if (! is_dir($path)) {
+                    mkdir($path, 0755, true);
+                }
+            }         
+        }
     }
 }
