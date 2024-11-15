b9736c73985a1acdc08982bbbf8ca6df89d262e5
kent <melquiadeskentcarlo@gmail.com>
2024-11-15 18:21:00 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
-    return 'cool';
+    return redirect('/admin');
 });
 
 Route::get('/clear-cache', function () {
