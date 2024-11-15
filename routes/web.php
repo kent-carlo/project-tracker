7bb8e3274b0c176ebcdc55143b8e33701ff10876
kent <melquiadeskentcarlo@gmail.com>
2024-11-15 16:55:17 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return 'cool';
 });
+
+Route::get('/clear-cache', function () {
+    Artisan::call('vendor:publish --force --tag=livewire:assets');
+});
